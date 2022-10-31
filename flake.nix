a95f47c01f806545dd57e445ea725f630e519b5e
Thomas Leonard <talex5@gmail.com>
2022-10-31 15:31:23 +0000

    Add minimal flake.nix

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = rec {
+          opam2nix = import ./nix { inherit pkgs; ocaml-ng = pkgs.ocaml-ng; };
+          default = opam2nix;
+        };
+      }
+    );
+}
